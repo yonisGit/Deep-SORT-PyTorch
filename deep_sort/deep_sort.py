ce41fa47119e4062c552ba204a63508cee606db1
YonisGithub <yoniel@post.bgu.ac.il>
2024-07-05 17:54:41 +0300

    WIP: added budget to config

--- deep_sort/deep_sort.py
+++ deep_sort/deep_sort.py
@@ -18,7 +18,7 @@ class DeepSort(object):
         self.extractor = Extractor(model_path, use_cuda=use_cuda)
 
         max_cosine_distance = max_dist
-        nn_budget = 100
+        nn_budget = TrackerConfig.NN_BUDGET
         metric = NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
         self.tracker = Tracker(metric)
 
--- deep_sort/tracker_config.py
+++ deep_sort/tracker_config.py
@@ -1,13 +1,27 @@
 class TrackerConfig(object):
     MIN_CONFIDENCE = 0.3
-    MAX_DIST = 0.3
+    MAX_DIST = 0.35
     MAX_IOU = 0.7
-    N_INIT = 6
-    MAX_AGE = 200
+    N_INIT = 10
+    MAX_AGE = 2000
+    NN_BUDGET = 1000
 
-#GOOD FOR VMD:
+# GOOD FOR VMD:
     # MIN_CONFIDENCE = 0.3
     # MAX_DIST = 0.3
     # MAX_IOU = 0.7
     # N_INIT = 6
     # MAX_AGE = 200
+
+    # MIN_CONFIDENCE = 0.3
+    # MAX_DIST = 0.25
+    # MAX_IOU = 0.7
+    # N_INIT = 30
+    # MAX_AGE = 2000
+
+# GOOD FOR DNC:
+#     MIN_CONFIDENCE = 0.3
+#     MAX_DIST = 0.205
+#     MAX_IOU = 0.8
+#     N_INIT = 15
+#     MAX_AGE = 200
